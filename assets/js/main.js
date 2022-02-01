7c5437b4bb2fe046e9062e25c820977c0d1208d1
akippo <98787788+akippo@users.noreply.github.com>
2022-02-01 12:33:22 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,16 @@
 let scroll = new SmoothScroll('a[href*="#"]', {
   header: '[data-scroll-header]'
 });
+
+  document.getElementById("header-time").innerHTML = showTime();
+  
+  function showTime() {
+    var now = new Date();
+    var nowhour = now.getHours();
+    var nowminutes = now.getMinutes();
+    var nowseconds = now.getSeconds();
+  
+    var text = nowhour + "：" + nowminutes + "：" + nowseconds; 
+    
+    return text;
+  }
