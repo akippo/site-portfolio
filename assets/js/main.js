e2a3e8be7b810f9f07c0974e951f29f2f8b02430
akippo <98787788+akippo@users.noreply.github.com>
2022-02-01 17:33:42 +0900

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,8 @@ let scroll = new SmoothScroll('a[href*="#"]', {
   header: '[data-scroll-header]'
 });
 
-  document.getElementById("top-time").innerHTML = showTime();
-  
-  function showTime() {
+  document.getElementById("top-time-hours").innerHTML = showTimeHours();
+  function showTimeHours() {
     let now = Date.now();
     let start = Date.parse('1990-10-30T12:18:00');
     let elapsed = now - start;
@@ -17,3 +16,28 @@ let scroll = new SmoothScroll('a[href*="#"]', {
     
     return text;
   }
+
+document.getElementById("top-time-minutes").innerHTML = showTimeMinutes();
+function showTimeMinutes() {
+  let now = Date.now();
+  let start = Date.parse('1990-10-30T12:18:00');
+  let elapsed = now - start;
+  const seconds = Math.floor(elapsed/1000)%60;
+  const minutes = Math.floor(elapsed/1000/60);
+
+  let text = minutes;
+
+  return text;
+}
+
+document.getElementById("top-time-seconds").innerHTML = showTimeSeconds();
+function showTimeSeconds() {
+  let now = Date.now();
+  let start = Date.parse('1990-10-30T12:18:00');
+  let elapsed = now - start;
+  const seconds = Math.floor(elapsed/1000);
+
+  let text = seconds;
+
+  return text;
+}
