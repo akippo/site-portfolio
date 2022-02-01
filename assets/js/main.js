07b0177a6df92f5a6c5a596c811006a0c8832cca
akippo <98787788+akippo@users.noreply.github.com>
2022-02-01 14:10:40 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,13 @@ let scroll = new SmoothScroll('a[href*="#"]', {
   
   function showTime() {
     let now = new Date();
-    let start = Date();
+    let start = Date.parse('30 Oct 1990 12:18:00');
+    let elapsed = now.getTime() - start.getTime() 
     let nowhour = now.getHours();
     let nowminutes = now.getMinutes();
     let nowseconds = now.getSeconds();
   
-    let text = nowhour + "：" + nowminutes + "：" + nowseconds; 
+    let text = nowhour + "：" + nowminutes + "：" + nowseconds + elapsed; 
     
     return text;
   }
